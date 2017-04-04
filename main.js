cbb5b1ce53d07136e931621a7e5a475514ebfb68
Josephine Gabrielle Baltazar <jgsbaltazar@yahoo.com>
2017-04-03 17:50:05 -0700

    added data

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
 $(document).ready(function(){
-	$.getJSON('https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop&callback=?', function(data){
-		var htmlStr = data.results.map(function(obj){
+	 $.getJSON("https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop&callback=?", function(data){
+	
+	var data = data.results.map(function(item){
+		return {
+			price: item.price,
+			shopname: item.Shop.shop_name,
+			title: item.title,
+			img: item.Images[0].url_570xN
+		}
+	})
+	var htmlStr = "";
 
-		// data.forEach(function(field){
-		// 	
+	// function showIcons() {
+	// 	$(this).addClass('hover')
+	// }, function () {
+	// 	$(this).removeClass('hover')
+	// }
+	data.forEach(function(item){
+		 htmlStr += `
+		<div class="pics">
+			<div><button id="heart"><img src="./assets/heart.png"/></button><button id="burger"><img src="./assets/hamburger.png"/></button><img src="${item.img}" /></div>
+
+			<div class="description"> 
+				<p class="about">${item.title}</p> 
+				<p class="shop">${item.shopname}</p>
+				<p class="price">${'$' + item.price}</p>
+			</div>
+		</div>
+		`
+	}) 
+	$("#mainCenter").html(htmlStr)
 })
 
+})
+
+
+	
+
+	
+
+	
